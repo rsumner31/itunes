41c3d383b38b87e1e6a7ac93b3eb4774c498bb58
Robert Sumner Jr <rsumner31@gmail.com>
2018-03-17 06:09:41 -0500

    initial commit
    Conflicts:
    	.gitignore
    	Rakefile
    	lib/itunes.rb
    	lib/itunes/version.rb

--- lib/itunes.rb
+++ lib/itunes.rb
@@ -1,3 +1,4 @@
+<<<<<<<+HEAD
 require 'faraday_middleware'
 require 'itunes/configuration'
 require 'itunes/client'
@@ -29,3 +30,38 @@ module ITunes
     new.respond_to?(method, include_private) || super(method, include_private)
   end
 end
+=======
+require 'httparty'
+
+class ITunes
+  include HTTParty
+  
+  base_uri 'ax.phobos.apple.com.edgesuite.net/WebObjects/MZStoreServices.woa/wa/ws'
+  format :json
+  
+  def method_missing(name, *args)
+    methods = %q{movie podcast music music_video audiobook short_film tv_show all}
+    
+    if methods.include?(name.to_s)    
+      camelcase = name.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+      camelcase[0] = camelcase[0].chr.downcase
+      
+      request(args.first, camelcase)
+    else
+      super(name, *args)
+    end
+  end
+  
+  private
+    def request(term, media='all')
+      raise ArgumentError, 'you need to search for something, provide a term.' if term.nil?
+      
+      self.class.get('Search', {
+        :query => {
+          :term => term,
+          :media => media
+        }
+      })
+    end
+end
+>>>>>>>-40bf18c
